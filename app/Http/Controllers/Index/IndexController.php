68418833164d58197482b0150e982b02d2a95681
LAPTOP-FTVIV820\Dave <dave>
2021-09-23 15:44:14 +0800

    增加laravel；

--- app/Http/Controllers/Index/IndexController.php
+++ app/Http/Controllers/Index/IndexController.php
@@ -15,9 +15,39 @@ class IndexController extends Controller
      * */
     public function Index()
     {
+        //init
         $viewLocation = "{$this->viewPrefix}.index";
 
+
+
         return view($viewLocation);
+    }
+
+    /**
+     * Get Blog Homepage Introduce Info
+     * @author dave
+     * */
+    private function getIntroduce():array
+    {
+
+    }
+
+    /**
+     * Get Blog Homepage Content Info
+     * @author dave
+     * */
+    private function getContent():array
+    {
+
+    }
+
+    /**
+     * Get Blog Homepage Brief Info
+     * @author dave
+     * */
+    private function getBrief():array
+    {
+
     }
 
 }
--- resources/views/index/index/index.blade.php
+++ resources/views/index/index/index.blade.php
@@ -4,9 +4,34 @@
         <meta charset="utf-8">
         <meta name="author" content="dave,">
     </head>
+    <style>
+        .home_page_introduce{
+            background-color: #6c757d;
+        }
+        .home_page_brief{
+            background-color: #545b62;
+        }
+    </style>
     <body>
-        <div class="container">
-
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-2 home_page_introduce">
+                    <img/>
+                    <h3>果然</h3>
+                    <p>php&python</p>
+                    <p>中国 深圳</p>
+                </div>
+                <div class="col-md-6 home_page_content">
+                    <h3>第二列</h3>
+                    <p>菜鸟教程</p>
+                    <p>学的不仅是技术，更是梦想！！！</p>
+                </div>
+                <div class="col-md-4 home_page_brief">
+                    <h3>第三列</h3>
+                    <p>菜鸟教程</p>
+                    <p>学的不仅是技术，更是梦想！！！</p>
+                </div>
+            </div>
         </div>
     </body>
 </html>
